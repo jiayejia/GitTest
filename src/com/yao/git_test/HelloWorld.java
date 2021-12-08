bb4b854186bfea81ec5af3eeb73555c7314f816b
YAO <1692989928@qq.com>
2021-12-08 15:44:57 +0800

    second commit

--- src/com/yao/git_test/HelloWorld.java
+++ src/com/yao/git_test/HelloWorld.java
@@ -3,5 +3,6 @@ package com.yao.git_test;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
